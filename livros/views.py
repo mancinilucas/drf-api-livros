ff0291aa271c34cba712ae9cf0a7d7ac4ba29c7a
mancinilucas <mancini.lucasg@gmail.com>
2024-08-09 11:11:05 -0300

    add: output personalizado de json a partir do db

--- livros/views.py
+++ livros/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
 
+from .models import Livro
 # Create your views here.
+
+
+def livros_view(request):
+    livros = Livro.objects.all()
+    output = [{
+        'nome': livro.nome,
+        'autor': livro.autor,
+        'categoria': livro.categoria
+    } for livro in livros]
+    return HttpResponse(json.dumps(output))
--- lojadelivros/urls.py
+++ lojadelivros/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from livros.views import livros_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('livros/', livros_view),
 ]
