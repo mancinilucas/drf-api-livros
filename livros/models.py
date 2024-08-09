eadcd5ddcb042b89475ab57a96f8d6e568f75333
mancinilucas <mancini.lucasg@gmail.com>
2024-08-08 22:21:12 -0300

    add: primeira migrations

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Livro(models.Model):
+    nome = models.CharField(max_length=30)
+    categoria = models.CharField(max_length=30)
+    autor = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.nome
